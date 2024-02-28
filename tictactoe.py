1c807e953fb24a890e4dda31c4ef4ba74a82b27a
Perry Xie <perryxie.nz@gmail.com>
2024-02-29 00:42:28 +1300

    Setting up a simple tic tac toe game.

---
+++ tictactoe.py
@@ -0,0 +1,10 @@
+cols = 3
+row1 = [" "]*cols
+row2 = [" "]*cols
+row3 = [" "]*cols
+
+print(str(row1[0]) + " | " + str(row1[1]) + " | " + str(row1[2]))
+print("----------")
+print(str(row2[0]) + " | " + str(row2[1]) + " | " + str(row2[2]))
+print("----------")
+print(str(row3[0]) + " | " + str(row3[1]) + " | " + str(row3[2]))
